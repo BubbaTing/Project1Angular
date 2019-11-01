59e97df632d47522443009757993870baa6eb238
BubbaTing <ronbubbating@hotmail.com>
2019-11-01 15:06:55 -0400

    adding some file

--- ProjectOne/src/app/app.module.ts
+++ ProjectOne/src/app/app.module.ts
@@ -3,14 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ManagerPageComponent } from './manager-page/manager-page.component';
 
+import { RequestsComponent } from './requests/requests.component';
 import { LoginComponent } from './login/login.component';
 import { TicketOptionComponent } from './ticket-option/ticket-option.component';
-
-import { ManagerPageComponent } from './manager-page/manager-page.component';
 import { RequestComponent } from './request/request.component';
 import { TicketsComponent } from './tickets/tickets.component';
-
 
 @NgModule({
   declarations: [
@@ -19,6 +18,7 @@ import { TicketsComponent } from './tickets/tickets.component';
     TicketOptionComponent,
     ManagerPageComponent,
     RequestComponent,
+    RequestsComponent,
     TicketsComponent
   ],
   imports: [
