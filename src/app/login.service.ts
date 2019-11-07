3b0ce4208d3546b39d99196b858bc154abb8d1a6
DanL12186 <DanL12186@users.noreply.github.com>
2019-11-06 22:46:53 -0500

    refactored login code

--- src/app/login.service.ts
+++ src/app/login.service.ts
@@ -7,34 +7,32 @@ import { UserData } from 'src/app/models/UserData';
 @Injectable({
   providedIn: 'root'
 })
+
 export class LoginService {
 
   currentlyLoggedIn = false;
 
   constructor(private router: Router, private httpClient: HttpClient) { }
 
-    //crate an obserable userData 
+  //create an obserable userData 
   private userSubject: Subject<UserData> = new Subject();
-  public $userData: Observable<UserData>
-    = this.userSubject.asObservable();
+  public $userData: Observable<UserData> = this.userSubject.asObservable();
     
-   async loginHttp(credentials: {username: string, password: string}) {
-    //debugger;
+  async loginHttp(credentials: {username: string, password: string}) {
+
     const loginCredentials = {
       username: credentials.username,
       password: credentials.password
-      
     };
-    const url = 'http://localhost:8081/project1/login';
-    console.log(loginCredentials);
-    const a = await this.httpClient.post(url, loginCredentials).toPromise();
-    let b = JSON.parse(JSON.stringify(a));
-    if(b.roleID === 1 || b.roleID === 2){
 
+    const url        = 'http://localhost:8080/project1/login';
+    const userObject = await this.httpClient.post(url, loginCredentials).toPromise();
+    const userString = JSON.stringify(userObject);
+    const userJSON   = JSON.parse(userString);
+
+    if (userJSON.roleID === 1 || userJSON.roleID === 2) {
       this.router.navigateByUrl('/ticket_option');
       this.currentlyLoggedIn = true;
-    } else {
-      this.currentlyLoggedIn =  false;
     }
     return this.currentlyLoggedIn;
   }
