68d138eab2610ddca585c0ca202d581fb8e52ddf
BubbaTing <ronbubbating@hotmail.com>
2019-11-07 20:37:51 -0500

    minor adjustment to the ticket component and user-details service

--- src/app/user-details.service.ts
+++ src/app/user-details.service.ts
@@ -8,14 +8,17 @@ import { tickets_details } from './viewTickets';
 })
 export class UserDetailsService {
 
-  private url: string ='http://localhost:8081/project1/response';
+  private url: string ='http://localhost:8080/project1/response';
 
   public tickets_details: any[]
 
   constructor(private http: HttpClient ) { }
 
   getDetails(): Observable<tickets_details[]>{
-
-    return this.http.get<tickets_details[]>(this.url);
+    const currentUser = {
+      author: 7
+    }
+   
+    return this.http.post<tickets_details[]>(this.url, currentUser);
   }
 }
